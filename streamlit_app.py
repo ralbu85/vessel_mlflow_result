c9180d61ea986ca6acdd1397c2794ef4458c7707
ralbu85 <38488565+ralbu85@users.noreply.github.com>
2025-05-24 07:14:28 +0000

    publish

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,25 @@
 import streamlit as st
-import matplotlib.pyplot as plt
-import seaborn as sns
-import pandas as pd
-
 import pandas as pd
 import plotly.express as px
+import os
 
-# Load your data
-df = pd.read_csv("Vessel_full_factorial_nowcast_Linear_stdscaler.csv")  # or however you load it
+# 1. List all CSV files in the `data/` folder
+data_dir = "data"
+csv_files = [f for f in os.listdir(data_dir) if f.endswith(".csv")]
+selected_file = st.selectbox("Select CSV file", csv_files)
+
+# 2. Load selected CSV file
+df = pd.read_csv(os.path.join(data_dir, selected_file))
+st.success(f"Loaded {selected_file} with {len(df)} rows")
+
+
 desired_order = ["none", "1h", "3h", "6h", "12h", "24h"]  # example
+factors = ['downsample', 'split', 'target_feature', 'OutlierRemoval', 'environmental_features']
+metrics = ["mae", "mape", "r2", "mse"]
 
 # Selectors
-color_feature = st.selectbox("Color By:", df.columns)
-metric = st.selectbox("Metric:", ["mae", "mape", "r2", "mse"])  # adjust as needed
+color_feature = st.selectbox("Color By:", factors)
+metric = st.selectbox("Metric:", metrics)  # adjust as needed
 
 # Hover info
 hover_factors = [
